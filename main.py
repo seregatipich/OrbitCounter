e0e7fb43c910e7d25d5d6acc1205feb0a3e71bdc
Sergei Poluektov <seregatipich@outlook.com>
2023-03-27 10:46:18 +0200

    removed unused import and fixed some PEP8 issues

--- main.py
+++ main.py
@@ -1,22 +1,21 @@
-import numpy as np
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 from scipy.integrate import solve_ivp
 
-# Гравитационная постоянная Земли (м^3/с^2)
+# Gravitational constant of the Earth (m^3/s^2)
 G = 6.67430e-11
 M = 5.97219e24
 
-# Начальные условия для МКС
-a = 6771000  # Большая полуось (м)
-e = 0.0015   # Эксцентриситет орбиты
+# ISS orbit details
+a = 6771000  # Major Axis (m)
+e = 0.0015   # Orbit eccentricity
 
-# Параметры орбиты
+# Calculation of orbit parameters
 h = np.sqrt(G * M * a * (1 - e**2))
 T = 2 * np.pi * np.sqrt(a**3 / (G * M))
 
-# Уравнения движения
-def orbit(t, y):
+# Equation of motion
+def orbit(y):
     x, y, z, vx, vy, vz = y
     r = np.sqrt(x**2 + y**2 + z**2)
     return [
@@ -28,21 +27,23 @@ def orbit(t, y):
         -G * M * z / r**3,
     ]
 
-# Начальные условия
+
+# Initial conditions
 r0 = a * (1 - e)
 v0 = h / r0
 
-# Начальные значения (x, y, z, vx, vy, vz)
+# Initial values (x, y, z, vx, vy, vz)
 initial_conditions = [r0, 0, 0, 0, v0, 0]
 
-# Решение системы дифференциальных уравнений
-sol = solve_ivp(orbit, (0, T), initial_conditions, rtol=1e-8, atol=1e-8, t_eval=np.linspace(0, T, 1000))
+# Solving a system of differential equations
+sol = solve_ivp(orbit, (0, T), initial_conditions, rtol=1e-8,
+                atol=1e-8, t_eval=np.linspace(0, T, 1000))
 
-# Создание 3D графика
+# Create 3D graphics
 fig = plt.figure(figsize=(10, 10))
 ax = fig.add_subplot(111, projection='3d')
 
-# Добавление Земли на график
+# Adding Earth to the graph
 earth_radius = 6371000
 u, v = np.mgrid[0:2*np.pi:50j, 0:np.pi:25j]
 x = earth_radius * np.cos(u) * np.sin(v)
@@ -50,12 +51,12 @@ y = earth_radius * np.sin(u) * np.sin(v)
 z = earth_radius * np.cos(v)
 ax.plot_surface(x, y, z, color='blue', alpha=0.3)
 
-# Добавление орбиты МКС
-ax.plot(sol.y[0], sol.y[1], sol.y[2], label='Орбита МКС')
+# Adding the ISS orbit
+ax.plot(sol.y[0], sol.y[1], sol.y[2], label='ISS Orbit')
 
 ax.set_xlabel('X (м)')
 ax.set_ylabel('Y (м)')
 ax.set_zlabel('Z (м)')
-ax.set_title('Орбита МКС и Земля в 3D')
+ax.set_title('ISS Orbit 3D')
 ax.legend()
 plt.show()
